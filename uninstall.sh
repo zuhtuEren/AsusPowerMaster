4ea43b8a3458a0b405fa2476f23873b4ab3ab3fb
Zühtü Eren İncekara <incekarazuhtu04@gmail.com>
2026-01-25 23:03:00 +0300

    feat: v2.6 Gold - uninstall.sh fix

--- uninstall.sh
+++ uninstall.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-# uninstall.sh - Asus Power Master v2.6 Complete Cleanup
+# Asus Power Master - Complete Uninstaller v2.6
 
-[[ $EUID -ne 0 ]] && echo -e "\e[31m[ERROR]\e[0m Please run with sudo." && exit 1
+[[ $EUID -ne 0 ]] && echo -e "\e[31m[ERROR]\e[0m Please run as root (sudo)." && exit 1
 
-echo "[1/3] Stopping and disabling services..."
-for svc in asus-pwr.service asus-pwr-persistence.service; do
-    systemctl stop $svc 2>/dev/null
-    systemctl disable $svc 2>/dev/null
-    rm -f /etc/systemd/system/$svc
-done
+echo -e "\e[34m[1/4]\e[0m Stopping system services..."
+systemctl stop asus-pwr.service 2>/dev/null
+systemctl disable asus-pwr.service 2>/dev/null
+systemctl stop asus-pwr-persistence.service 2>/dev/null
+systemctl disable asus-pwr-persistence.service 2>/dev/null
+
+echo -e "\e[34m[2/4]\e[0m Removing system files..."
+rm -f /usr/local/bin/asus-pwr
+rm -rf /usr/local/share/asus-pwr/
+rm -f /etc/systemd/system/asus-pwr.service
+rm -f /etc/systemd/system/asus-pwr-persistence.service
+
+echo -e "\e[34m[3/4]\e[0m Cleaning up configuration files..."
+rm -f /etc/asus-power.conf 
+
+echo -e "\e[34m[4/4]\e[0m Reloading system daemons..."
 systemctl daemon-reload
 
-echo "[2/3] Removing files and directories..."
-rm -f /usr/local/bin/asus-pwr
-rm -rf /usr/local/share/asus-pwr
-rm -f /etc/asus-power.conf
-
-echo "[3/3] Cleaning system cache..."
-hash -r
-
-echo -e "\n\e[32m[DONE]\e[0m Asus Power Master has been completely removed from the system."
+echo -e "\n\e[32m[DONE]\e[0m Asus Power Master has been completely removed from your system."
